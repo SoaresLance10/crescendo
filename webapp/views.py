3e73173307d69fa29b7ac60b6f8cd1b8869ce419
Lance Soares <soareslance10@gmail.com>
2021-03-08 17:41:30 +0530

    hackathon page

--- webapp/views.py
+++ webapp/views.py
@@ -250,7 +250,7 @@ def mechathon(request):
     return render(request, "webapp/mechathon.html", {"title": "Mechathon", "top_message": "Crescendo Mechathon 2k21"})
 
 def valorant(request, id):
-    return render(request, "webapp/valorant.html", {"title": "Valorant", "top_message": "E-Sports Valorant"})
+    return render(request, "webapp/valorant.html", {"title": "Valorant", "top_message": "E-Sports Valorant", "id": id})
 
 def cod(request, id):
-    return render(request, "webapp/cod.html", {"title": "Call Of Duty", "top_message": "E-Sports Call of Duty Mobile"})
+    return render(request, "webapp/cod.html", {"title": "Call Of Duty", "top_message": "E-Sports Call of Duty Mobile", "id": id})
